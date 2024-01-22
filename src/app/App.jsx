2a446c38ef2b616728c240446ef7b7052a65b81b
Romartiny <romortiny@gmail.com>
2024-01-22 15:25:35 +0300

    update awomy.com

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import './assets/styles/App.scss';
 import { AppRouter } from './providers/router';
 import ConnectionDetector from '../components/ConnectionDetector';
-import {HeaderBlock} from "../components/Header/HeaderBlock";
 
 function App () {
   return (
     <ConnectionDetector>
-        <HeaderBlock />
       <AppRouter/>
     </ConnectionDetector>
   );
