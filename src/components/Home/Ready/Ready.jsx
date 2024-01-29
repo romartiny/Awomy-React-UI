4b2a82205c2c6262b8f790f5f0cf09b90df3ee16
Romartiny <romortiny@gmail.com>
2024-01-29 14:21:30 +0300

    update awomy.com

--- src/components/Home/Ready/Ready.jsx
+++ src/components/Home/Ready/Ready.jsx
@@ -8,12 +8,12 @@ const Ready = () => {
     return (
         <section className="relative bg-black z-20">
             <Container className="py-12 sm:py-16">
-                <div className="flex flex-col items-center bg-gray-900/60 rounded-2xl px-7 py-16">
+                <div className="flex flex-col items-center bg-gray-900/60 rounded-2xl px-5 py-9 lg:py-16 lg:px-7">
                     <div className="flex flex-col items-center mt-5">
                         <Title title={readyData.title} className="text-2xl lg:text-3xl text-center" />
                         <span className="text-sm lg:text-xl text-gray-500 mt-5 text-center">{readyData.description}</span>
                     </div>
-                    <div className="md:flex md:items-center mt-6 mb-5">
+                    <div className="mt-6 mb-5 md:flex md:items-center">
                         <Link to={readyData.signUp.href} className="text-white bg-pink-600 hover:bg-pink-700 font-bold rounded-2xl text-xs lg:text-sm px-5 py-2.5 transition ease-in-out delay-70 duration-300 focus:outline-none">{readyData.signUp.title}</Link>
                         <Link to={readyData.learnMore.href} className="text-white/80 border border-gray-600 hover:border-gray-500 hover:text-white font-bold rounded-2xl text-xs lg:text-sm px-5 py-2.5 ml-6 transition duration-300 ease-in-out delay-70 focus:outline-none">{readyData.learnMore.title}</Link>
                     </div>
